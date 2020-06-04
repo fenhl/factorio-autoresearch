a77b667000d3b420f0346144ae9d073927631291
Someone <someone@somenet.org>
2020-06-05 01:14:58 +0200

    Should work now with space exploration. Thanks Ferlonas.

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -7,9 +7,9 @@ for _, tool in pairs(data.raw.tool) do
                 name = "auto_research_tool_" .. tool.name,
                 filename = tool.icon or (tool.icons[1] and tool.icons[1].icon) or nil,
                 priority = "extra-high-no-scale",
-                width = tool.icon_size,
-                height = tool.icon_size,
-                scalse = 32/tool.icon_size
+                width = tool.icon_size or (tool.icons[1].icon_size) or 32,
+                height = tool.icon_size or (tool.icons[1].icon_size) or 32,
+                scale = 32/(tool.icon_size or (tool.icons[1].icon_size) or 32)
             }
         })
     end
