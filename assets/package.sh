9b41331101b1f0a43ec5e2402f837fd1b86e82dd
Fenhl <fenhl@fenhl.net>
2026-02-14 22:43:12 +0000

    package: Exit on error

--- assets/package.sh
+++ assets/package.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 version=$(jq -r '.version' info.json)
 rm -f autoresearch-fenhl_*.zip
 git archive --prefix "autoresearch-fenhl_${version}/" -o "autoresearch-fenhl_${version}.zip" HEAD
